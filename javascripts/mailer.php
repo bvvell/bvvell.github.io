bef57201306169122fb0f830b72f4fa4eeec25da
vb <vb@gm.co>
2014-08-30 15:39:22 +0300

    test mail

--- javascripts/mailer.php
+++ javascripts/mailer.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: http://bvvell.github.io");
+?>
+<?php
     // My modifications to mailer script from:
     // http://blog.teamtreehouse.com/create-ajax-contact-form
     // Added input sanitizing to prevent injection
@@ -37,17 +40,14 @@
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
-            http_response_code(200);
             echo "Thank You! Your message has been sent.";
         } else {
             // Set a 500 (internal server error) response code.
-            http_response_code(500);
             echo "Oops! Something went wrong and we couldn't send your message.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
         echo "There was a problem with your submission, please try again.";
     }
 
