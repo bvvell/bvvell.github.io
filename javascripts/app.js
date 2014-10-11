b6cef6144c654fb8c0524ec781bf6e786882bb96
Vladimir Bernacky <vvbernacky@gmail.com>
2014-10-11 19:17:18 +0300

    add some fix

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,1 +1,38 @@
-$(function(){$(".contact").on("click",function(){$(".popUp").show();$(".highlight").show();$("html").addClass("pop");return false});$(".close").on("click",function(){$(".popUp").hide();$(".highlight").hide();$("html").removeClass("pop");return false});var e=$("#ajax-contact");var t=$("#form-messages");$(e).submit(function(n){n.preventDefault();var r=$(e).serialize();$.ajax({type:"POST",url:$(e).attr("action"),data:r}).done(function(e){$(t).removeClass("error");$(t).addClass("success");$(t).text(e);$("#name").val("");$("#email").val("");$("#message").val("");ga("send","event","formsubmit","submit")}).fail(function(e){$(t).removeClass("success");$(t).addClass("error");if(e.responseText!==""){$(t).text(e.responseText)}else{$(t).text("Oops! An error occured and your message could not be sent.")}})})})
+$(function () {
+    $(".contact").on("click", function () {
+        $(".popUp").show();
+        $(".highlight").show();
+        $("html").addClass("pop");
+        return false;
+    });
+    $(".close").on("click", function () {
+        $(".popUp").hide();
+        $(".highlight").hide();
+        $("html").removeClass("pop");
+        $("#form-messages").text("").hide();
+        return false
+    });
+    var e = $("#ajax-contact");
+    var t = $("#form-messages");
+    $(e).submit(function (n) {
+        n.preventDefault();
+        var r = $(e).serialize();
+        $.ajax({type: "POST", url: $(e).attr("action"), data: r}).done(function (e) {
+            $(t).removeClass("error");
+            $(t).addClass("success");
+            $(t).text(e);
+            $("#name").val("");
+            $("#email").val("");
+            $("#message").val("");
+            ga("send", "event", "formsubmit", "submit")
+        }).fail(function (e) {
+            $(t).removeClass("success");
+            $(t).addClass("error");
+            if (e.responseText !== "") {
+                $(t).text(e.responseText)
+            } else {
+                $(t).text("Oops! An error occured and your message could not be sent.").show();
+            }
+        })
+    })
+})
